9f6cfe5a3867c665739d8d8b8344652b83572351
Logan Sauer <logan9590@gmail.com>
2020-04-22 21:31:16 -0500

    removed private

--- server-express-mongodb/private/mongo.js
+++
@@ -1,9 +0,0 @@
-
-
-
-var mongoDB = "mongodb://127.0.0.1/database";
-//var mongoDB =
-  "mongodb+srv://ammon:Password1%21@cluster0-lhvh5.mongodb.net/test?retryWrites=true&w=majority";
-
-
-module.exports = mongoDB;
