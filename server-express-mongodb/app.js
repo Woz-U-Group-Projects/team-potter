53395c428d456b5d45a43b3b55ff31189986bb24
Malik Felix <malikfelix50@gmail.com>
2020-04-24 19:15:29 -0400

    added mongo.js private file

--- server-express-mongodb/app.js
+++ server-express-mongodb/app.js
@@ -9,6 +9,7 @@ var tasksRouter = require("./routes/tasks");
 
 var app = express();
 
+var mongoDB = require("./private/mongo");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,11 +19,6 @@ app.use(cors());
 
 app.use("/tasks", tasksRouter);
 
-var mongoDB = "mongodb://127.0.0.1/database";
-//var mongoDB =
-  "mongodb+srv://ammon:Password1%21@cluster0-lhvh5.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("connected", () => console.log(`Mongoose connection open to ${mongoDB}`));
 db.on("disconnected", () => console.log("Mongoose connection disconnected"));
---
+++ server-express-mongodb/private/mongo.js
@@ -0,0 +1,5 @@
+var mongoDB = "mongodb://127.0.0.1/database";
+//var mongoDB =
+  "mongodb+srv://dbuser:W9wB4S0xgvap4ccx@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
