f069d22126d1159015b7d94d674ad07ac09c4223
Malik Felix <malikfelix50@gmail.com>
2020-05-06 17:03:33 -0400

    Removed merge headers in mongo.js

--- server-express-mongodb/private/mongo.js
+++ server-express-mongodb/private/mongo.js
@@ -1,37 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-||||||| merged common ancestors
-<<<<<<<<< Temporary merge branch 1
-//var mongoDB = "mongodb://127.0.0.1/database";
-var mongoDB =
-  "mongodb+srv://dbuser:W9wB4S0xgvap4ccx@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
-
-module.exports = mongoDB;
-||||||||| merged common ancestors
-=========
-=======
->>>>>>> c1e29c90b200f4e59ed03b2e260a98d62fd2bc80
 // var mongoDB = "mongodb://127.0.0.1/database";
  var mongoDB =
   "mongodb+srv://dbuser1:Password1@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = mongoDB;
 
-<<<<<<< HEAD
-||||||| merged common ancestors
-var mongoDB = "mongodb://127.0.0.1/database";
-//var mongoDB =
-  "mongodb+srv://dbuser:W9wB4S0xgvap4ccx@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-=======
-//var mongoDB = "mongodb://127.0.0.1/database";
-var mongoDB =
-  "mongodb+srv://dbuser:W9wB4S0xgvap4ccx@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
+// //var mongoDB = "mongodb://127.0.0.1/database";
+// var mongoDB =
+//   "mongodb+srv://dbuser:W9wB4S0xgvap4ccx@lifestyle-blog-vjztb.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports = mongoDB;
->>>>>>> d4edfa22a5b13d11a8a31d5b16c7fc2dcc2f6e35
-||||||| merged common ancestors
->>>>>>>>> Temporary merge branch 2
-=======
->>>>>>> c1e29c90b200f4e59ed03b2e260a98d62fd2bc80
+// module.exports = mongoDB;
+// ||||||| merged common ancestors
+// >>>>>>>>> Temporary merge branch 2
+// =======
+// >>>>>>> c1e29c90b200f4e59ed03b2e260a98d62fd2bc80
