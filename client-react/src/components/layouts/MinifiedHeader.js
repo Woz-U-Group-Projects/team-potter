e34b51c695274f1beea1bec69a724f381fd89ce6
Malik Felix <malikfelix50@gmail.com>
2020-05-08 15:30:59 -0400

    adds MinifiedHeader.js

---
+++ client-react/src/components/layouts/MinifiedHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MinifiedHeader = ({ title }) => (
+  <div>
+    <nav id="mainNavbar" className="navbar bg-dark navbar-dark navbar-expand fixed-top">
+      <div className="container">
+        <ul className="navbar-nav">
+          <li className="nabar nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/AddArticle" className="nav-link">Add Article</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+
+export default MinifiedHeader;
