dbdf507174b0067fb8dd09165fee0432e0ce96de
njorogedk <njoroge.info@gmail.com>
2020-05-13 18:14:13 -0500

    Update component

--- client-react/src/components/Article.js
+++ client-react/src/components/Article.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
-// import{ Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import{ Link } from "react-router-dom";
+// import { Button } from "react-bootstrap";
 import "../task.min.css";
 import MinifiedHeader from "./layouts/MinifiedHeader";
 import Footer from "./layouts/Footer";
@@ -59,13 +59,13 @@ class Article extends React.Component {
 
           <span className="badge badge-secondary p-2">{article.authorname}</span>
 
-          <button type="button" className="btn btn-primary">
+          <Link className="btn btn-primary" to={`/EditArticle/${article._id}`}>
             Edit
-          </button>
+          </Link>
 
-          <Button onClick={this.deleteData}type="button" className="btn btn-danger" href='/'>
+          <Link onClick={this.deleteData}className="btn btn-danger" to='/'>
             Delete
-          </Button>
+          </Link>
           
           
             <div className="form-group"></div>
