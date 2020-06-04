66c730f1fb7b25e241c4d5915892bb68feb05512
Dickson K. Njoroge <njorogdk@courts.state.mo.us>
2020-06-04 15:04:13 -0500

    profile

--- client-react/src/components/profiles/Dickson.js
+++ client-react/src/components/profiles/Dickson.js
@@ -17,15 +17,23 @@ class Dickson extends React.Component {
               <h4 class="subtitle">SD Student</h4>
             </div>
 
-            <div>
-              <h2>Bio</h2> 
+            <div> 
               <p>
-                Full Stack JavaScript developement student, web developer, currently living in Columbia, MO. I have a Bachelor of Science in Information Technology from Jomo Kenyatta University of Agriculture and Technology (JKUAT), 
-              </p>
-              <h2>Experience</h2> 
+              Full Stack JavaScript student, currently living in Columbia, MO. 
+              I have a Bachelor of Science in Information Technology from Jomo Kenyatta University of Agriculture and Technology (JKUAT).
+              </p> 
               <p>
-                Full Stack JavaScript student, web developer, currently living in Columbia, MO. I have a Bachelor of Science in Information Technology from Jomo Kenyatta University of Agriculture and Technology(JKUAT), 
+              I am a husband and a father of one six years old daughter. 
+              I love technology very much, I have worked as a systems/network administrator for 8 years at a university in Kenya. 
+              Currently working as IT technologist at Boone Courthouse in Columbia MO. 
+              I been doing software development on need basis, just getting systems do what I need at that without following any standards. 
+              During my previous employment I had the opportunity to implement all open source network management and monitoring system, KOHA as the library system and Moodle for E-learning. 
+              My technology passion enabled me customize and configure these software’s to meet the institutional needs.
               </p>
+              <p>I decided to further my passions and become a profession software developer. 
+                I declared my interests to friends and one friend whom is also a WozU graduate proposed to me WozU as that dream college I was looking for. 
+                I am happy that finally for accomplishing this step in my career path.
+                </p>
               <ul class="social-links">
                 <li>
                   <a href="https://github.com/njorogedk">Github</a>
